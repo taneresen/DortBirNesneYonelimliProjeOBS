10bc81817ee53a96d3262b3d4bb30846c1063cad
taner <taner@MSI>
2020-04-04 21:14:58 +0200

    Java OOP

---
+++ DortBirNesneYonelimliProjeOBS/src/com/bilisimegitim/obs/Calistir.java
@@ -0,0 +1,13 @@
+package com.bilisimegitim.obs;
+
+public class Calistir {
+
+	public static void main(String[] args) {
+		
+		Ogrenci ogr = new Ogrenci (); // Ögrenci klasindan obje yarattik.
+		ogr.selamVer();
+		
+
+	}
+
+}
---
+++ DortBirNesneYonelimliProjeOBS/src/com/bilisimegitim/obs/Ogrenci.java
@@ -0,0 +1,13 @@
+package com.bilisimegitim.obs;
+
+public class Ogrenci {
+	
+	public void selamVer()  //Void Hic bir islem yapmiyor ve döndurmuyor.Sadece ekrana yadiriyor.
+	{
+		
+		System.out.println("Merhaba");
+		
+	}
+	
+
+}
